cce58d80434e377c4dce5073ddd08046a74425c7
Antonio Barbosa <acrb1108@gmail.com>
2023-07-10 20:45:51 -0300

    Projeto criado

---
+++ ProjetoAula01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+//define a localização da classe dentro do projeto
+namespace ProjetoAula01
+{
+    //definição da classe
+    class Program
+    {
+        //método (funções) para executar o projeto
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
